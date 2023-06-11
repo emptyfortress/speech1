3bf8d25b987b88db3a8ddd68ed6e93c11d28a6f8
Volkov Alexander <volkov.525@gmail.com>
2023-06-11 13:17:46 +0300

    made filtering works

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,22 +1,51 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, reactive, ref, watchEffect } from 'vue'
 import WordHighlighter from 'vue-word-highlighter'
 
 const help = defineModel<boolean>({ required: true, default: false })
-const filter = ref()
+const filter = ref('')
 
-const sections = [
+const sections = reactive([
 	{
-		id: 0, icon: "mdi-book-open-page-variant-outline", label: 'Ключевые слова', children: [
+		id: 0, icon: "mdi-book-open-page-variant-outline", expand: false, label: 'Ключевые слова', children: [
 			{ id: 0, label: 'Как создать свою библиотеку?', action: '' },
 			{ id: 1, label: 'Как добавить слово в библиотеку?', action: '' },
 		]
 	},
-	{ id: 1, icon: "mdi-finance", label: 'Отчеты' },
-	{ id: 2, icon: "mdi-toy-brick-search-outline", label: 'Запросы' },
-	{ id: 3, icon: "mdi-check-all", label: 'Чек-листы' },
-	{ id: 4, icon: "mdi-lan", label: 'Темы' },
-]
+	{ id: 1, icon: "mdi-finance", expand: false, label: 'Отчеты' },
+	{ id: 2, icon: "mdi-toy-brick-search-outline", expand: false, label: 'Запросы' },
+	{ id: 3, icon: "mdi-check-all", expand: false, label: 'Чек-листы' },
+	{ id: 4, icon: "mdi-lan", expand: false, label: 'Темы' },
+])
+
+const filterByLabel = (array: any, searchTerm: string) => {
+	return array.reduce((prev: any, curr: any) => {
+		const children = curr.children ? filterByLabel(curr.children, searchTerm) : undefined
+
+		return curr.label?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			curr.description?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+			children?.length > 0
+			? [...prev, { ...curr, children }]
+			: prev
+	}, [])
+}
+
+const filtered = computed(() => {
+	if (filter.value.length > 1) {
+		return filterByLabel(sections, filter.value)
+	}
+	return sections
+})
+
+const expandAll = (() => {
+	return sections.map(sec => sec.expand = true)
+})
+
+watchEffect(() => {
+	if (filter.value.length > 1) {
+		expandAll()
+	}
+})
 </script>
 
 <template lang="pug">
@@ -34,7 +63,7 @@ q-dialog(v-model="help" persistent)
 		q-card-section
 			q-scroll-area.scroll
 				q-list.q-mx-md
-					q-expansion-item(v-for="sec in sections" :key="sec.id" expand-separator :label="sec.label" :icon="sec.icon")
+					q-expansion-item(v-model="sec.expand" v-for="sec in filtered" :key="sec.id" :label="sec.label" :icon="sec.icon")
 						q-card
 							q-card-section(v-if="sec.children")
 								q-list
