865b8333f16dd7b7c29758be485f0767b1282d38
Volkov Alexander <volkov.525@gmail.com>
2023-06-19 09:58:08 +0300

    fix bug on dashboard with apexchart

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -2,11 +2,11 @@
 .gridcat
 	.cat
 		.zag.cursor-pointer
-			|{{catselect}}
+			|{{ catselect }}
 			q-icon(name="mdi-menu-down")
 			q-menu
 				q-list
-					q-item(clickable v-close-popup v-for="item in cat.categories" :key="item.id" @click="catselect = item.label" :class="{'selected' : item.label === catselect}")
+					q-item(clickable v-close-popup v-for="item in cat.categories" :key="item.id" @click="catselect = item.label" :class="{ 'selected': item.label === catselect }")
 						q-item-section
 							q-item-label {{ item.label }}
 		q-btn(flat round icon="mdi-dots-horizontal")
@@ -19,12 +19,12 @@
 					q-item(clickable v-close-popup v-for="category in cat.categories" :key="category.id")
 						q-item-section
 							q-checkbox(:model-value="category.selected" :label="category.label" dense @update:model-value="update(category)")
-	component(:is="ChipCalendar1" label="Текущий месяц").right
+	component.right(:is="ChipCalendar1" label="Текущий месяц")
 	.cat
 		component(:is="CategoryTable")
 
 	q-card
-		component(:is="VueApexCharts" type="bar" height="102%" :options="coolOptions" :series="coolSeries")
+		VueApexCharts(type="bar" height="102%" :options="coolOptions" :series="coolSeries")
 
 </template>
 
@@ -67,18 +67,18 @@ const coolOptions = {
 		colors: ['#fff'],
 	},
 	xaxis: {
-		categories: [
-			'Продажи',
-			'Сервис',
-			'Оплата',
-			'Жалобы',
-			'Неисправности',
-			'Услуги',
-			'Работа',
-			'Регионы',
-			'Грубость',
-			'Остальное',
-		],
+		// categories: [
+		// 	'Продажи',
+		// 	'Сервис',
+		// 	'Оплата',
+		// 	'Жалобы',
+		// 	'Неисправности',
+		// 	'Услуги',
+		// 	'Работа',
+		// 	'Регионы',
+		// 	'Грубость',
+		// 	'Остальное',
+		// ],
 		labels: {
 			show: false,
 			minHeight: 0,
@@ -130,56 +130,71 @@ const catselect = ref('Неисправности')
 	grid-template-columns: repeat(4, 1fr);
 	column-gap: 1rem;
 	align-content: start;
+
 	.cat {
 		grid-column: 1/4;
 		display: flex;
 		justify-content: space-between;
 	}
 }
+
 .q-table th {
 	font-weight: 300;
 }
+
 .q-table tr {
 	cursor: pointer;
+
 	&.sel {
 		background: #def0fd;
 	}
+
 	td {
 		font-weight: 600;
 		font-size: 16px;
+
 		.up {
 			margin-left: 0.5rem;
 			color: green;
 		}
+
 		.down {
 			margin-left: 0.5rem;
 			color: darkred;
 		}
+
 		.legend {
 			width: 10px;
 			height: 10px;
+
 			&.blue {
 				background: #249efa;
 			}
+
 			&.green {
 				background: #24e6a4;
 			}
+
 			&.orange {
 				background: #fdba3a;
 			}
 		}
+
 		&.graph:hover {
 			outline: 1px solid #249efa;
 		}
 	}
+
 	td:nth-child(2) {
 		font-weight: 400;
 		font-size: 13px;
 	}
 }
+
 .right {
 	justify-self: end;
 }
+
 .selected {
 	background: $bgSelection;
 	color: $blue-9;
