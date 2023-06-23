17a1a8e8ba0f0d180cc27cc6857702c1be0dc3d1
Volkov Alexander <volkov.525@gmail.com>
2023-06-23 09:43:03 +0300

    all works

--- src/components/common/FilterSelect.vue
+++ src/components/common/FilterSelect.vue
@@ -1,79 +1,40 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import type { Ref } from 'vue'
 
-const props = defineProps({
-	options: {
-		type: Array,
-		required: true,
-		default: []
-	}
+interface Props {
+	options: string[]
+}
+const props = defineProps<Props>()
+const modelValue = defineModel<string[]>({ default: [] })
+
+const toggle = ((opt: string) => {
+	const index = modelValue.value.indexOf(opt)
+	if (index > -1) {
+		modelValue.value?.splice(index, 1)
+	} else modelValue.value?.push(opt)
 })
-
-const modelValue = defineModel<string[]>()
-
-// const selected = computed(() => {
-// 	return props.options
-// })
-const selected = ref([])
-
-// const selection: Ref<String[]> = ref([])
-
-const toggle = ((opt: string, ind: number) => {
-	modelValue.value?.push(opt)
+const selClass = ((e: string) => {
+	const temp = modelValue.value.find(item => item === e)
+	if (temp) {
+		return 'selected'
+	}
 })
 </script>
 
 <template lang="pug">
-p {{ modelValue }}
-q-btn(flat round icon="mdi-filter"  @click="action" dense size="sm") 
+q-btn(flat round icon="mdi-filter"  @click="" dense size="sm") 
 	q-menu
 		q-list
-			q-item(v-for="(opt, index) in options" clickable @click="toggle(opt, index)")
+			q-item(v-for="opt in props.options" key="opt" clickable @click="toggle(opt)" :class="selClass(opt)")
 				q-item-section(side)
-					q-checkbox(v-model="selected" dense :val="opt")
+					q-checkbox(:model-value="modelValue" dense :val="opt" @click="toggle(opt)")
 				q-item-section
 					q-item-label {{ opt }}
-q-item-se
-//
-// q-select(v-model="modelValue"
-// 	:options="props.options"
-// 	standout
-// 	multiple
-// 	map-options
-// 	emit-value
-// 	dense
-// 	filled)
-//
-// 	template(v-slot:option="{ itemProps, opt, selected, toggleOption }")
-// 		q-item(v-bind="itemProps")
-// 			q-item-section(side)
-// 				q-checkbox(:modelValue="selected" dense @update:model-value="toggleOption(opt)")
-// 			q-item-section
-// 				q-item-label {{ opt }}
 </template>
 
 <style scoped lang="scss">
-.q-select {
-	font-size: .7rem;
-}
+@import '@/assets/styles/myvariables.scss';
 
-:deep(.q-field--auto-height.q-field--dense .q-field__control),
-:deep(.q-field--auto-height.q-field--dense .q-field__native) {
-	min-height: 28px;
-}
-
-:deep(.q-field--dense .q-field__marginal) {
-	height: 30px;
-}
-
-:deep(.q-field--filled .q-field__control) {
-	padding: 0 3px;
-	background: rgba(255, 255, 255, .7);
-}
-
-:deep(.q-field--dense .q-field__after),
-:deep(.q-field--dense .q-field__append) {
-	padding-left: 0;
+.selected {
+	background: $bgSelection;
 }
 </style>
