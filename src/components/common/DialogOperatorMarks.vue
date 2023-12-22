0f4525602876a9b3cbe93429c9704dc4c5cf85f6
Volkov Alexander <volkov.525@gmail.com>
2023-12-22 15:50:29 +0300

    dialog with marks

--- src/components/common/DialogOperatorMarks.vue
+++ src/components/common/DialogOperatorMarks.vue
@@ -3,10 +3,19 @@ q-dialog(v-model="modelValue")
 	q-card(style="width: 600px; max-width: 80vw;")
 		q-btn.close(round color="negative" icon="mdi-close" v-close-popup)
 		q-card-section
-			.text-h6 Карточка оценки
+			.text-h6 {{ mark.text}}
 		q-card-section
-			p Что здесь должно быть? Нужны пояснения...
-			pre {{ props.mark }}
+			q-list(dense)
+				q-item(v-for="item in 5")
+					q-item-section(side) Параметр {{ item }}:
+					q-item-section.text-weight-bold {{ mark.mark }}
+					q-item-section Это комментарий.
+			.total Общая оценка - {{ mark.mark }}
+			div Записи по которым производилась оценка:
+			.links Петров_2023-10-03
+			.links Петров_2023-10-04
+					
+
 		q-card-section
 			q-card-actions(align="right")
 				q-btn(flat color="negative" label="Я не согласен!" v-close-popup) 
@@ -22,4 +31,18 @@ const props = defineProps<{ mark: Mark }>()
 const modelValue = defineModel()
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.total {
+	margin-top: 1rem;
+	margin-bottom: 1rem;
+	text-align: center;
+	border-top: 1px solid #ccc;
+	border-bottom: 1px solid #ccc;
+	padding: 0.3rem;
+	background: #eee;
+}
+.links {
+	cursor: pointer;
+	color: $primary;
+}
+</style>
