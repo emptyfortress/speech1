3149bbb082938d6c5f60bba7d3e28e786f99d632
Volkov Alexander <volkov.525@gmail.com>
2024-03-03 13:04:06 +0300

    add comments to profile op

--- src/components/common/DialogOperatorMarks.vue
+++ src/components/common/DialogOperatorMarks.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import EvalQuestions from '@/components/evaluate/EvalQuestions.vue'
+import Speech from '@/components/Speech.vue'
+import EvalComments from '@/components/evaluate/EvalComments.vue'
 
 const props = defineProps<{ anketa: Anketa }>()
 
 const modelValue = defineModel()
 const tab = ref('anketa')
+const sound = ref(50)
 </script>
 
 <template lang="pug">
@@ -29,11 +32,22 @@ q-dialog(v-model="modelValue")
 					q-scroll-area
 						EvalQuestions(operator)
 				q-tab-panel(name="records")
-					q-scroll-area
-						p recrord
+					.myplayer
+						q-linear-progress(:value=".6" color="positive")
+						.player
+							q-btn(round flat icon="mdi-rewind" @click.stop)
+							q-btn(round flat icon="mdi-pause" @click.stop)
+							q-btn(round flat icon="mdi-fast-forward" @click.stop)
+						.time 02:31
+						.row.items-center
+							q-icon(name="mdi-volume-medium" size="sm")
+							q-slider.slide(color="primary" v-model="sound")
+							q-icon(name="mdi-volume-high" size="sm")
+					q-scroll-area.dark
+						Speech
 				q-tab-panel(name="comments")
 					q-scroll-area
-						p comments
+						EvalComments
 					
 		q-card-section
 			q-card-actions(align="right")
@@ -68,5 +82,47 @@ q-dialog(v-model="modelValue")
 }
 .q-scrollarea {
 	height: 61vh;
+}
+.myplayer {
+	position: relative;
+	height: 50px;
+	background: $blue-grey-9;
+	color: #6d8e9e;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	padding-left: 1rem;
+	gap: 2rem;
+	font-size: 0.9rem;
+
+	.q-linear-progress {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	.player {
+		color: white;
+		display: flex;
+		justify-items: flex-start;
+		align-items: center;
+		height: 100%;
+	}
+
+	.time {
+		font-size: 2rem;
+		font-weight: lighter;
+		letter-spacing: 1px;
+		color: white;
+	}
+
+	.slide {
+		width: 150px;
+	}
+}
+.dark {
+	margin-top: 1px;
+	background: $blue-grey-9;
+	height: calc(61vh - 50px);
 }
 </style>
