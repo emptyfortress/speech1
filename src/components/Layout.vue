0a6a9c1cb63281a836b65fd3747f3fb4c55eca3e
Volkov Alexander <volkov.525@gmail.com>
2024-04-01 22:03:16 +0300

    calc width-height of widget

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 import { uid } from 'quasar'
 import { GridItem, GridLayout } from 'vue-ts-responsive-grid-layout'
+import SetupWidgetDialog from '@/components/dash/SetupWidgetDialog.vue'
+import { useElementSize } from '@vueuse/core'
 
-const layout = reactive([{ x: 0, y: 0, w: 3, h: 3, i: '0' }])
+const layout = reactive([{ x: 0, y: 0, w: 3, h: 3, i: 'el' }])
 const remove = (e: number) => {
 	let temp = [...document.getElementsByClassName('vue-grid-item')]
 	temp.forEach((el) => el.classList.add('move'))
@@ -17,13 +19,27 @@ const add = () => {
 	tmp.i = uid()
 	layout.push(tmp)
 }
+
+const dialog = ref(false)
+
+const setup = (e: string) => {
+	// dialog.value = !dialog.value
+	console.log(e)
+	console.log(cardRef.value)
+	console.log(width.value)
+}
+
+const cardRef = ref([])
+const { width, height } = useElementSize(cardRef.value)
 </script>
 
 <template lang="pug">
 q-page(padding)
 	.container
 		q-btn.fab(round color="primary" icon="mdi-plus" @click="add" size="lg") 
-		component(:is="GridLayout"
+
+		component(
+			:is="GridLayout"
 			:layout.sync="layout"
 			:col-num="12"
 			:row-height="30"
@@ -35,7 +51,8 @@ q-page(padding)
 			:show-close-button="false"
 			:use-css-transforms="true")
 
-			component(:is="GridItem" v-for="( item, index ) in layout"
+			component(:is="GridItem"
+				v-for="( item, index ) in layout"
 				:enableEditMode="editMode"
 				:x="item.x"
 				:y="item.y"
@@ -44,10 +61,13 @@ q-page(padding)
 				:i="item.i"
 				:show-close-button="false"
 				:key="item.i")
-				q-card
-					q-card-section {{item.i}}
+				q-card(ref="cardRef")
+					q-card-section
+						q-btn(flat color="primary" label="Настроить" @click="setup(item.i)" size="sm") 
 					q-icon.close(name="mdi-close" @click="remove(index)" dense)
 					q-icon.resize(name="mdi-resize-bottom-right" @click="" dense size="16px") 
+
+	SetupWidgetDialog(v-model="dialog")
 </template>
 
 <style scoped lang="scss">
@@ -83,5 +103,8 @@ q-page(padding)
 :deep(.vue-draggable-dragging) {
 	z-index: 1000;
 	box-shadow: 3px 3px 17px rgba(0, 0, 0, 0.4);
+}
+.fab {
+	z-index: 1;
 }
 </style>
---
+++ src/components/dash/SetupWidgetDialog.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const modelValue = defineModel()
+
+const splitterModel = ref(25)
+const hei = computed(() => {
+	return 'height: ' + (window.innerHeight - 205) + 'px;'
+})
+</script>
+
+<template lang="pug">
+q-dialog(v-model="modelValue" persistent maximized transition-show="slide-up" transition-hide="slide-down")
+	.cnt
+		.tip Настройте виджет или выберите готовый из библиотеки
+		.zg
+			q-icon(name="mdi-widgets-outline" size="26px")
+			span Конструктор виджетов
+		q-splitter(v-model="splitterModel" :limits="[0, 100]" :style="hei" @update:model-value="split")
+			template(v-slot:before)
+				.div before
+			template(v-slot:after)
+				.div after
+		q-card-actions(align="center")
+			q-btn(flat color="primary" label="Отмена" v-close-popup) 
+			q-btn(unelevated color="primary" label="Отмена" v-close-popup) 
+</template>
+
+<style scoped lang="scss">
+.cnt {
+	background: moz-linear-gradient(top, #d8e3f1 0%, #f4e8f4 52.6%, #fdf5e5 100%);
+	background: webkit-linear-gradient(top, #d8e3f1 0%, #f4e8f4 52.6%, #fdf5e5 100%);
+	background: linear-gradient(180deg, #d8e3f1 0%, #f4e8f4 52.6%, #fdf5e5 100%);
+	background-attachment: fixed;
+	// .content {
+	// 	max-width: 1200px;
+	// 	margin: 0 auto;
+	// }
+}
+
+.zg {
+	margin-top: 2rem;
+	font-size: 1.3rem;
+	text-align: center;
+	vertical-align: baseline;
+	.q-icon {
+		margin-right: 1rem;
+		transform: translateY(-5px);
+	}
+}
+.tip {
+	background: #9db2c3;
+	padding: 2px 2rem;
+	font-size: 1rem;
+	text-align: center;
+}
+.div {
+	height: 200px;
+}
+</style>
