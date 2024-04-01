6b71eb354ba9d8e46572c41c28c9783bc05afad3
Volkov Alexander <volkov.525@gmail.com>
2024-04-01 14:39:48 +0300

    uniq uid

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,41 +1,28 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
+import { uid } from 'quasar'
 import { GridItem, GridLayout } from 'vue-ts-responsive-grid-layout'
 
-const layout = reactive([
-	{ x: 0, y: 0, w: 2, h: 2, i: '0' },
-	{ x: 2, y: 0, w: 2, h: 4, i: '1' },
-	{ x: 4, y: 0, w: 2, h: 5, i: '2' },
-	{ x: 6, y: 0, w: 2, h: 3, i: '3' },
-	{ x: 8, y: 0, w: 2, h: 3, i: '4' },
-	{ x: 10, y: 0, w: 2, h: 3, i: '5' },
-	{ x: 0, y: 5, w: 2, h: 5, i: '6' },
-	{ x: 2, y: 5, w: 2, h: 5, i: '7' },
-	{ x: 4, y: 5, w: 2, h: 5, i: '8' },
-	{ x: 6, y: 3, w: 2, h: 4, i: '9' },
-	{ x: 8, y: 4, w: 2, h: 4, i: '10' },
-	{ x: 10, y: 4, w: 2, h: 4, i: '11' },
-	{ x: 0, y: 10, w: 2, h: 5, i: '12' },
-	{ x: 2, y: 10, w: 2, h: 5, i: '13' },
-	{ x: 4, y: 8, w: 2, h: 4, i: '14' },
-	{ x: 6, y: 8, w: 2, h: 4, i: '15' },
-	{ x: 8, y: 10, w: 2, h: 5, i: '16' },
-	{ x: 10, y: 4, w: 2, h: 2, i: '17' },
-	{ x: 0, y: 9, w: 2, h: 3, i: '18' },
-	{ x: 2, y: 6, w: 2, h: 2, i: '19' },
-])
+const layout = reactive([{ x: 0, y: 0, w: 3, h: 3, i: '0' }])
 const remove = (e: number) => {
 	let temp = [...document.getElementsByClassName('vue-grid-item')]
 	temp.forEach((el) => el.classList.add('move'))
 	layout.splice(e, 1)
 }
 const editMode = ref(true)
+
+const date = new Date()
+const add = () => {
+	const tmp = { ...layout.at(-1) }
+	tmp.i = uid()
+	layout.push(tmp)
+}
 </script>
 
 <template lang="pug">
 q-page(padding)
 	.container
-		q-btn.fab(round color="primary" icon="mdi-plus" @click="action" size="lg") 
+		q-btn.fab(round color="primary" icon="mdi-plus" @click="add" size="lg") 
 		component(:is="GridLayout"
 			:layout.sync="layout"
 			:col-num="12"
@@ -56,8 +43,7 @@ q-page(padding)
 				:h="item.h"
 				:i="item.i"
 				:show-close-button="false"
-				:key="item.i"
-				)
+				:key="item.i")
 				q-card
 					q-card-section {{item.i}}
 					q-icon.close(name="mdi-close" @click="remove(index)" dense)
