14b1a4610c7b65a2a887c6017c35a82c080149fe
Volkov Alexander <volkov.525@gmail.com>
2024-04-01 13:32:18 +0300

    add fab btn and resize icon

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -3,28 +3,10 @@ import { ref, reactive } from 'vue'
 import { GridItem, GridLayout } from 'vue-ts-responsive-grid-layout'
 
 const layout = reactive([
-	{ x: 0, y: 0, w: 2, h: 2, i: '0' },
-	{ x: 2, y: 0, w: 2, h: 4, i: '1' },
-	{ x: 4, y: 0, w: 2, h: 5, i: '2' },
-	{ x: 6, y: 0, w: 2, h: 3, i: '3' },
-	{ x: 8, y: 0, w: 2, h: 3, i: '4' },
-	{ x: 10, y: 0, w: 2, h: 3, i: '5' },
-	{ x: 0, y: 5, w: 2, h: 5, i: '6' },
-	{ x: 2, y: 5, w: 2, h: 5, i: '7' },
-	{ x: 4, y: 5, w: 2, h: 5, i: '8' },
-	{ x: 6, y: 3, w: 2, h: 4, i: '9' },
-	{ x: 8, y: 4, w: 2, h: 4, i: '10' },
-	{ x: 10, y: 4, w: 2, h: 4, i: '11' },
-	{ x: 0, y: 10, w: 2, h: 5, i: '12' },
-	{ x: 2, y: 10, w: 2, h: 5, i: '13' },
-	{ x: 4, y: 8, w: 2, h: 4, i: '14' },
-	{ x: 6, y: 8, w: 2, h: 4, i: '15' },
-	{ x: 8, y: 10, w: 2, h: 5, i: '16' },
-	{ x: 10, y: 4, w: 2, h: 2, i: '17' },
-	{ x: 0, y: 9, w: 2, h: 3, i: '18' },
-	{ x: 2, y: 6, w: 2, h: 2, i: '19' },
+	{ x: 0, y: 0, w: 3, h: 4, i: '0' },
+	{ x: 3, y: 0, w: 3, h: 4, i: '1' },
 ])
-const test = (e: number) => {
+const remove = (e: number) => {
 	let temp = [...document.getElementsByClassName('vue-grid-item')]
 	temp.forEach((el) => el.classList.add('move'))
 	layout.splice(e, 1)
@@ -33,34 +15,42 @@ const editMode = ref(true)
 </script>
 
 <template lang="pug">
-grid-layout(
-	:layout.sync="layout"
-	:col-num="12"
-	:row-height="30"
-	:is-draggable="true"
-	:is-resizable="true"
-	:is-mirrored="false"
-	:vertical-compact="true"
-	:margin="[10, 10]"
-	:show-close-button="false"
-	:use-css-transforms="true")
+q-page(padding)
+	.container
+		GridLayout(
+			:layout.sync="layout"
+			:col-num="12"
+			:row-height="30"
+			:is-draggable="true"
+			:is-resizable="true"
+			:is-mirrored="false"
+			:vertical-compact="true"
+			:margin="[10, 10]"
+			:show-close-button="false"
+			:use-css-transforms="true")
 
-	grid-item(v-for="( item, index ) in layout"
-		:enableEditMode="editMode"
-		:x="item.x"
-		:y="item.y"
-		:w="item.w"
-		:h="item.h"
-		:i="item.i"
-		:show-close-button="false"
-		:key="item.i"
-		)
-		q-card
-			q-card-section {{item.i}}
-			q-icon(name="mdi-close" @click="test(index)" dense) 
+			GridItem(v-for="( item, index ) in layout"
+				:enableEditMode="editMode"
+				:x="item.x"
+				:y="item.y"
+				:w="item.w"
+				:h="item.h"
+				:i="item.i"
+				:show-close-button="false"
+				:key="item.i"
+				)
+				q-card
+					q-icon.close(name="mdi-close" @click="remove(index)" dense size="14px") 
+					q-icon.resize(name="mdi-resize-bottom-right" @click="" dense size="16px" ) 
+					q-card-section {{item.i}}
+q-btn.fab(round color="primary" icon="mdi-plus" @click="" size="lg") 
 </template>
 
 <style scoped lang="scss">
+.container {
+	background: pink;
+	padding: 0;
+}
 .move {
 	transition: 0.2s ease all;
 }
@@ -68,10 +58,16 @@ grid-layout(
 	touch-action: none;
 	position: relative;
 }
-.q-icon {
+.close {
 	position: absolute;
 	right: 3px;
 	top: 3px;
+	cursor: pointer;
+}
+.resize {
+	position: absolute;
+	right: 3px;
+	bottom: 3px;
 	cursor: pointer;
 }
 .q-card {
@@ -87,5 +83,10 @@ grid-layout(
 :deep(.vue-draggable-dragging) {
 	z-index: 1000;
 	box-shadow: 3px 3px 17px rgba(0, 0, 0, 0.4);
+}
+.fab {
+	position: fixed;
+	bottom: 2rem;
+	right: 2rem;
 }
 </style>
