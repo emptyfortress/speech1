0ad163fbdcdd189ce24bdd553a3a53d84795158b
Volkov Alexander <volkov.525@gmail.com>
2024-04-14 17:10:51 +0300

    add zag

--- src/components/dash/WidgetTabs.vue
+++ src/components/dash/WidgetTabs.vue
@@ -7,12 +7,15 @@ const tabs = ref('data')
 <template lang="pug">
 q-tabs.q-mt-lg(v-model="tabs" align="left" active-color="primary")
 	q-tab(name="data" label="Данные")
+	q-tab(name="zag" label="Заголовок")
 	q-tab(name="style" label="Оформление")
 	q-tab(name="color" label="Цвет")
 
 q-tab-panels(v-model="tabs" animated)
 	q-tab-panel(name="data")
 		p Здесь настраиваем данные, задаем источник
+	q-tab-panel(name="zag")
+		p Здесь настраиваем заголовок
 	q-tab-panel(name="style")
 		p Здесь настраиваем шрифты, легенды и тп
 	q-tab-panel(name="color")
