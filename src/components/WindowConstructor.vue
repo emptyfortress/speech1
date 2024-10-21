6bfd9374ef276af75f225aa9a4835d9c02463a9b
Volkov Alexander <volkov.525@gmail.com>
2024-10-21 13:21:49 +0300

    add gpt request ot check-lists

--- src/components/WindowConstructor.vue
+++ src/components/WindowConstructor.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useLogic } from '@/stores/logic'
 import { useCheck } from '@/stores/check'
 import { useQuasar } from 'quasar'
@@ -39,25 +40,35 @@ const save = () => {
 		position: 'top-right',
 	})
 }
+const tabs = ref('logic')
+const gptRequest = ref()
 </script>
 
 <template lang="pug">
 q-dialog(v-model="logic.inception")
 	q-card
-		q-card-section.row.items-baseline.q-pb-none
-			q-space
-			#zg1(contenteditable @blur="update") {{ logic.currentLogic.label }}
-			q-space
-			q-btn(icon="mdi-close" flat round dense v-close-popup)
-
-		.q-mt-sm.q-mx-lg
-			#comment1(contenteditable @blur="updatecomment") {{logic.currentLogic.comment}}
-
 		q-card-section
-			component(:is="Puzzle")
+			q-btn.close(icon="mdi-close" round dense v-close-popup color="negative")
+			#zg1(contenteditable @blur="update") {{ logic.currentLogic.label }}
+			.q-mx-lg
+				#comment1(contenteditable @blur="updatecomment") {{ logic.currentLogic.comment }}
+			.row.justify-between
+				q-tabs(v-model="tabs" inline-label indicator-color="primary" active-color="primary" dense align="left")
+					q-tab(name="logic" label="Конструктор запросов")
+					q-tab(name="gpt" alert="red" label="Запрос на естественном языке")
+
+			q-tab-panels(v-model="tabs")
+				q-tab-panel(name="logic")
+					component(:is="Puzzle")
+				q-tab-panel(name="gpt")
+					q-input(v-model="gptRequest" type='textarea' filled placeholder="Расскажите, что Вас интересует")
 
 		q-card-section
 			q-card-actions(align="right")
+				.experiment
+					q-badge.q-mr-sm(color="red" rounded)
+					span Экспериментальная функция. Может быть изменена или удалена в будущем.
+				q-space
 				q-btn(flat label="Отмена" color="primary" v-close-popup)
 				q-btn(unelevated color="primary" icon="mdi-content-save-outline" label="Применить" @click="save")
 		
@@ -65,37 +76,54 @@ q-dialog(v-model="logic.inception")
 
 <style scoped lang="scss">
 @import '@/assets/styles/myvariables.scss';
-.q-dialog__inner--minimized > div {
+
+.q-dialog__inner--minimized>div {
 	min-width: 70%;
 }
+
 #zg1 {
 	font-size: 1rem;
 	text-transform: uppercase;
 	font-weight: 600;
 	padding: 0.5rem;
 	padding-bottom: 0;
+	text-align: center;
+
 	&:hover {
 		background: $bgLight;
 	}
+
 	&:focus {
 		outline: none;
 		border-bottom: 1px dotted $primary;
 		background: $bgSelection;
 	}
 }
+
 #comment1 {
 	font-size: 0.9rem;
-	margin-top: 1rem;
+	// margin-top: 1rem;
 	padding: 0.5rem;
 	padding-bottom: 0;
+	margin-bottom: 1rem;
+
 	/* text-align: center; */
 	&:hover {
 		background: $bgLight;
 	}
+
 	&:focus {
 		outline: none;
 		border-bottom: 1px dotted $primary;
 		background: $bgSelection;
 	}
+}
+
+:deep(.q-tab__alert) {
+	right: -16px;
+}
+
+.experiment {
+	font-size: .8rem;
 }
 </style>
