7627ecbab33056086bd541cc524ecfc05102a9d3
Volkov Alexander <volkov.525@gmail.com>
2025-01-24 13:28:35 +0300

    fix chart.js error remove venn

--- src/components/PodcatChart.vue
+++ src/components/PodcatChart.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { Chart, LinearScale } from 'chart.js'
-import { VennDiagramController, ArcSlice } from 'chartjs-chart-venn'
+// import { Chart, LinearScale } from 'chart.js'
+// import { VennDiagramController, ArcSlice } from 'chartjs-chart-venn'
 // import { VennDiagramChart } from 'chartjs-chart-venn'
 
 // Define the structure of the Venn data
@@ -11,59 +11,59 @@ import { VennDiagramController, ArcSlice } from 'chartjs-chart-venn'
 // }
 
 // Register the Venn diagram controller and necessary elements
-Chart.register(VennDiagramController, ArcSlice, LinearScale);
+// Chart.register(VennDiagramController, ArcSlice, LinearScale);
 
 // Function to render the Venn chart
 
 const canvas = ref()
 // Call renderChart when the component is mounted
-onMounted(() => {
-	const renderChart = () => {
-		const ctx = canvas.value.getContext('2d');
-
-		// Ensure context is available
-		if (!ctx) {
-			console.error('Failed to get canvas context');
-			return;
-		}
-
-		const vennData = [
-			{ sets: ['A'], size: 10 },
-			{ sets: ['B'], size: 20 },
-			{ sets: ['C'], size: 30 },
-			{ sets: ['A', 'B'], size: 5 },
-			{ sets: ['A', 'C'], size: 15 },
-			{ sets: ['B', 'C'], size: 10 },
-			{ sets: ['A', 'B', 'C'], size: 3 }
-		];
-
-		// new Chart(ctx, {
-		// 	// type: VennDiagramController.id,
-		// 	type: VennDiagramController.id,
-		// 	data: {
-		// 		datasets: [{
-		// 			data: vennData,
-		// 		}]
-		// 	},
-		// 	options: {
-		// 		responsive: true,
-		// 		maintainAspectRatio: false,
-		// 		plugins: {
-		// 			tooltip: {
-		// 				callbacks: {
-		// 					label(tooltipItem) {
-		// 						return `${tooltipItem.label}: ${tooltipItem.raw.size}`;
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-		// });
-	}
-
-	// renderChart()
-
-});
+// onMounted(() => {
+// 	const renderChart = () => {
+// 		const ctx = canvas.value.getContext('2d');
+//
+// 		// Ensure context is available
+// 		if (!ctx) {
+// 			console.error('Failed to get canvas context');
+// 			return;
+// 		}
+//
+// 		const vennData = [
+// 			{ sets: ['A'], size: 10 },
+// 			{ sets: ['B'], size: 20 },
+// 			{ sets: ['C'], size: 30 },
+// 			{ sets: ['A', 'B'], size: 5 },
+// 			{ sets: ['A', 'C'], size: 15 },
+// 			{ sets: ['B', 'C'], size: 10 },
+// 			{ sets: ['A', 'B', 'C'], size: 3 }
+// 		];
+//
+// 		// new Chart(ctx, {
+// 		// 	// type: VennDiagramController.id,
+// 		// 	type: VennDiagramController.id,
+// 		// 	data: {
+// 		// 		datasets: [{
+// 		// 			data: vennData,
+// 		// 		}]
+// 		// 	},
+// 		// 	options: {
+// 		// 		responsive: true,
+// 		// 		maintainAspectRatio: false,
+// 		// 		plugins: {
+// 		// 			tooltip: {
+// 		// 				callbacks: {
+// 		// 					label(tooltipItem) {
+// 		// 						return `${tooltipItem.label}: ${tooltipItem.raw.size}`;
+// 		// 					}
+// 		// 				}
+// 		// 			}
+// 		// 		}
+// 		// 	}
+// 		// });
+// 	}
+//
+// 	// renderChart()
+//
+// });
 
 </script>
 
